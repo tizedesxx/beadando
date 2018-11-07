5e25a1517d382812987ca3bcd29c7e37a75c32c7
Tizedes Zoltán <tizedeszoltan97@gmail.com>
2018-11-07 23:54:10 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function openItem(evt, itemName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(itemName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").Click();
